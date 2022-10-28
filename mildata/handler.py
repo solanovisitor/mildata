6a3cd80c7a1ef0a9125ad63c46c56bb4897f623c
solanovisitor <solano.todeschini@gmail.com>
2022-10-28 18:22:55 +0000

    adding get_benefs

--- mildata/handler.py
+++ mildata/handler.py
@@ -1,6 +1,7 @@
 # A collection of functions to interact with our SQL Server
 
-import pyodbc
+from sqlalchemy.engine import URL
+from sqlalchemy import create_engine
 import pandas as pd
 import numpy as np
 import os
@@ -9,24 +10,26 @@ import datetime
 import time
 
 # A class to pull data from our clients SQL Server
-class Pull():
+class CreateEngine:
     # Initialize the class
     def __init__(self, server, database, username, password):
         self.server = server
         self.database = database
         self.username = username
         self.password = password
-        self.cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + self.server + ';DATABASE=' + self.database + ';UID=' + self.username + ';PWD=' + self.password)
-        self.cursor = self.cnxn.cursor()
+        self.connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+ self.password
+        self.connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": self.connection_string})
+        self.engine = create_engine(connection_url)
 
 # An inherited class from Pull to filter data from our clients SQL Server
-class Beneficiarios(Pull):
+class Beneficiarios(CreateEngine):
+    self.query = "SELECT * FROM dw.Beneficiarios"
     # Initialize the class
     def __init__(self, server, database, username, password):
         super().__init__(server, database, username, password)
 
     # Filter data from the SQL Server
-    def filter(self, query):
-        self.cursor.execute(query)
-        return self.cursor.fetchall()
+    def get_benefs(self):
+        self.benef = pd.read_sql_query('''SELECT * FROM [blkch].[aux_blkch_beneficiarios]''', self.engine)
+        return self.benef
 
