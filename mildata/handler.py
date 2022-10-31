769686d42c81ba9b5e10c58724ad7039c013d3e8
solanovisitor <solano.todeschini@gmail.com>
2022-10-31 18:17:15 +0000

    pulling creds from yaml

--- mildata/handler.py
+++ mildata/handler.py
@@ -1,7 +1,9 @@
 # A collection of functions to interact with our SQL Server
+from abc import abstractmethod
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
 import pandas as pd
+import yaml
 
 # A class to pull data from our clients SQL Server
 class Engine:
@@ -17,6 +19,18 @@ class Engine:
     def connect(self):
         self.engine = create_engine(self.connection_url)
         return self.engine
+
+    # An abstract method for opening YAML with credentials
+
+    @abstractmethod
+    def load_credentials(path):
+        with open(path, 'r') as stream:
+            try:
+                parsed_creds=yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return parsed_creds
+
 
 # A class to pull data from our clients SQL Server
 class Beneficiarios:
--- mildata/script.py
+++ mildata/script.py
@@ -1,6 +1,16 @@
 from handler import Engine, Beneficiarios
+import yaml
 
-datalake = Engine(server=server, database=database, username=username, password=password)
+
+def load_credentials(path):
+    with open(path, 'r') as stream:
+        try:
+            parsed_creds=yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    return parsed_creds
+
+datalake = Engine(server=parsed_creds['server'], database=parsed_creds['database'], username=parsed_creds['username'], password=parsed_creds['password'])
 connection = Engine.connect(datalake)
 sample = Beneficiarios(connection=connection, regional='ES', email='True', celular='True', n='1000')
 
