20f438e98d15c3cb6b811bc6dad88b878f0af04a
solanovisitor <solano.todeschini@gmail.com>
2022-10-31 17:46:28 +0000

    inserting credentials

--- mildata/script.py
+++
@@ -1,8 +0,0 @@
-from multiprocessing import connection
-from handler import Engine, Beneficiarios
-server = 'srv-sql-med-interop.database.windows.net'
-database = 'med-dw-interop'
-username = 'az-dw-user'
-password = 'ZhBgmV*$Ts2r!cNX'
-datalake = Engine(server=server, database=database, username=username, password=password)
-connection = Engine.connect(datalake)
