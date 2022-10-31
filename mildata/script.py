20ca148d6768b67ba74de91abb4ec6f2a245b930
solanovisitor <solano.todeschini@gmail.com>
2022-10-31 18:19:59 +0000

    pulling creds from yaml

--- mildata/script.py
+++ mildata/script.py
@@ -1,15 +1,7 @@
 from handler import Engine, Beneficiarios
-import yaml
 
-
-def load_credentials(path):
-    with open(path, 'r') as stream:
-        try:
-            parsed_creds=yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-    return parsed_creds
-
+path_to_credentials = '/home/dsazureuser/mildata/mildata/credentials/creds.yaml'
+parsed_creds = Engine.load_credentials(path_to_credentials)
 datalake = Engine(server=parsed_creds['server'], database=parsed_creds['database'], username=parsed_creds['username'], password=parsed_creds['password'])
 connection = Engine.connect(datalake)
 sample = Beneficiarios(connection=connection, regional='ES', email='True', celular='True', n='1000')
